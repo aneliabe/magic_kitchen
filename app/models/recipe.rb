45969313c649434ee84250d332b8322864fcec18
Aneli Abe <abe.aneli@gmail.com>
2026-03-09 10:29:32 -0300

    implemented openAI gem to generate the recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,35 @@ class Recipe < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
+
+  after_save :set_content, if: -> { saved_change_to_name? || saved_change_to_ingredient? }
+
+  def content
+    if super.blank?
+      set_content
+    else
+      super
+    end
+  end
+
+  def set_content
+    client = ::OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: "Give me a simple recipe for #{name} with the ingredients #{ingredient}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."}]
+    })
+    new_content = chatgpt_response["choices"][0]["message"]["content"]
+
+    update(content: new_content)
+    return new_content
+  end
+
+  # def content
+  #   client = ::OpenAI::Client.new
+  #   chatgpt_response = client.chat(parameters: {
+  #     model: "gpt-4o-mini",
+  #     messages: [{ role: "user", content: "Give me a simple recipe for #{name} with the ingredients #{ingredient}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."}]
+  #   })
+  #   return chatgpt_response["choices"][0]["message"]["content"]
+  # end
 end
